91960b2378d699cca1f97691add88c23c353b484
MeGaDeTH91 <megadeth_91@abv.bg>
2018-02-25 15:16:43 +0200

    CommandFolderAdded

---
+++ BashSoft/BashSoft/IO/Commands/OpenFileCommand.cs
@@ -0,0 +1,20 @@
+﻿using SimpleJudge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BashSoft.IO.Commands
+{
+    public class OpenFileCommand : Command
+    {
+        public OpenFileCommand(string input, string[] data, Tester judge, StudentsRepository repository, IOManager inputOutputManager) : base(input, data, judge, repository, inputOutputManager)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
