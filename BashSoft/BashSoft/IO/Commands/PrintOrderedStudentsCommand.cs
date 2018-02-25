ae9f62d8de84ba22a1e24aa896854729e0143eb3
MeGaDeTH91 <megadeth_91@abv.bg>
2018-02-25 16:13:59 +0200

    CommandsAdded

---
+++ BashSoft/BashSoft/IO/Commands/PrintOrderedStudentsCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using SimpleJudge;
+using BashSoft.Exceptions;
+
+namespace BashSoft.IO.Commands
+{
+    public class PrintOrderedStudentsCommand : Command
+    {
+        public PrintOrderedStudentsCommand(string input, string[] data, Tester judge, StudentsRepository repository, IOManager inputOutputManager) : base(input, data, judge, repository, inputOutputManager)
+        {
+        }
+
+        public override void Execute()
+        {
+            if (this.Data.Length != 5)
+            {
+                throw new InvalidCommandException(this.Input);
+            }
+            string courseName = this.Data[1];
+            string comparison = this.Data[2].ToLower();
+            string takeCommand = this.Data[3].ToLower();
+            string takeQuantity = this.Data[4].ToLower();
+
+            this.TryParseParametersForOrderAndTake(takeCommand, takeQuantity, courseName, comparison);
+        }
+        private void TryParseParametersForOrderAndTake(string takeCommand, string takeQuantity, string courseName, string comparison)
+        {
+            if (takeCommand == "take")
+            {
+                if (takeQuantity == "all")
+                {
+                    this.Repository.OrderAndTake(courseName, comparison, null);
+                }
+                else
+                {
+                    int studentsToTake;
+                    var hasParsed = int.TryParse(takeQuantity, out studentsToTake);
+                    if (hasParsed)
+                    {
+                        this.Repository.OrderAndTake(courseName, comparison, studentsToTake);
+                    }
+                    else
+                    {
+                        throw new ArgumentException(ExceptionMessages.InvalidTakeQuantityParameter);
+                    }
+                }
+            }
+            else
+            {
+                throw new InvalidCommandException(this.Input);
+            }
+        }
+    }
+}
